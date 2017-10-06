19f6af658b063762a857a48e16e869f524314c90
Novell <novell4ick@gmail.com>
2017-10-06 14:11:34 +0500

    Добавлено наследование от IMongoModel.

--- DataModels/Models/Team.cs
+++ DataModels/Models/Team.cs
@@ -1,19 +1,49 @@
 ﻿using System.Collections.Generic;
+using DataModels.Helpers;
+using DataModels.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace DataModels.Models
 {
     /// <summary>
     /// Класс описывает команду.
     /// </summary>
-    public class Team
+    public sealed class Team : IMongoModel
     {
         /// <summary>
         /// Инициализирует новый экземпляр класса <see cref="Team"/>.
         /// </summary>
-        public Team()
+        /// <param name="name">Название команды.</param>
+        public Team(string name)
         {
+            this.Name = name;
             this.Players = new HashSet<Player>();
         }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Team"/>.
+        /// </summary>
+        /// <param name="id">Идентификатор в MongoDB.</param>
+        public Team(ObjectId id)
+        {
+            this.Id = id;
+        }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Team"/>.
+        /// </summary>
+        internal Team()
+        {
+        }
+
+        /// <summary>
+        /// Идентификатор в MongoDB.
+        /// </summary>
+        [BsonId]
+        [JsonConverter(typeof(BsonObjectIdConverter))]
+        public ObjectId Id { get; set; }
 
         /// <summary>
         /// Получает или задает название команды.
